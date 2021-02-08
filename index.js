da66e39c016122d75a9fc2828cf82309cb3c2431
kelvincao <kelvincao6@gmail.com>
2021-02-07 20:20:11 -0800

    added connection to osu api

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+
+/**
+ * Import Discord token, command prefix, and Osu access token
+ */
+const { token, prefix, osu_access_token } = require("./config.json");
 const client = new Discord.Client();
-require('dotenv').config();
 
-const TOKEN = process.env.TOKEN
+/**
+ * node-fetch library used to get API requests
+ */
+const fetch = require("node-fetch");
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.channel.send('pong');
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  console.log("test");
+
+  if (command === "map") {
+    const mapID = args[0];
+    const url = new URL(`https://osu.ppy.sh/api/v2/beatmaps/${mapID}/scores`);
+
+    /**
+     * Optional params to include
+     */
+    // let params = {
+    //   country: "JP",
+    //   filter: "all",
+    //   variant: "4k",
+    // };
+
+    // Object.keys(params).forEach((key) =>
+    //   url.searchParams.append(key, params[key])
+    // );
+
+    let headers = {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + osu_access_token,
+    };
+
+    fetch(url, {
+      method: "GET",
+      headers: headers,
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        json.scores.forEach((score) => {
+          message.channel.send(score.id);
+        });
+      });
   }
 });
 
-client.login(TOKEN);
+client.login(token);
